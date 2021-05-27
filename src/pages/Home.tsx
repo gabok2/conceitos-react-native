f45d55bd3fcb68eec8400eb55a2a39f015986b4b
gabriel <gabrielbld9@gmail.com>
2021-05-27 14:01:37 -0300

    second commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,6 +15,8 @@ export function Home() {
 
   function handleAddTask(newTaskTitle: string) {
 
+    if (!newTaskTitle) return;
+
     const data = {
       id: new Date().getTime(),
       title: newTaskTitle,
